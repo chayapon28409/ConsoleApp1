ce13c753273f25d7216358918cea5384132e3e11
Borm'com EN <bromza_22@hotmail.com>
2019-05-28 16:50:46 +0700

    Update2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Test 123");
+            Console.WriteLine("Test 123111");
         }
     }
 }
